186632ea859f5dd71f1c9face2f04fd72d89e8e2
najeza7 <72994208+najeza7@users.noreply.github.com>
2021-01-18 17:19:53 -0600

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,18 +6,20 @@ import csv
 # Set path
 csvpath = os.path.join(".","Resources","election_data.csv")
 
-total_votes = 0
-candidates = []
-kvotes = 0
-cvotes = 0
-ovotes = 0
-lvotes = 0
+# Initialize and define variables we will need
+total_votes = 0 #variable to count the total number of votes
+candidates = [] #listo to save the column of candidates
+kvotes = 0 #variable to store Khan votes
+cvotes = 0 #variable to store Correy votes
+ovotes = 0 #variable to store O'Tooley votes
+lvotes = 0 #variable to store Li votes
 
-# Open the cvs file
+# Open the csv file
 with open(csvpath, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csvheader = next(csvreader)
 
+    # Loop to calculate total number of votes, total votes for each candidate and save candidates column
     for row in csvreader:
         total_votes +=1
         candidates.append(row[2])
@@ -30,18 +32,18 @@ with open(csvpath, "r") as csvfile:
         else:
             lvotes += 1
 
-w_name = ["O'Tooley" , "Correy", "Khan", "Li"] 
-w_votes = [ovotes, cvotes, kvotes, lvotes]
-ls_cand = list(set(candidates))
-total_votes = ovotes + cvotes + kvotes + lvotes
-opercentage = "{:.3f}".format((ovotes/total_votes)*100)
-kpercentage = "{:.3f}".format((kvotes/total_votes)*100)
-lpercentage = "{:.3f}".format((lvotes/total_votes)*100)
-cpercentage = "{:.3f}".format((cvotes/total_votes)*100)
-winner = max(w_votes)
-i_winner = w_votes.index(winner)
 
+ls_cand = list(set(candidates)) # List of the candidates who received votes
+w_name = ["O'Tooley" , "Correy", "Khan", "Li"] # List with the names of the candidates
+w_votes = [ovotes, cvotes, kvotes, lvotes] # List with the total votes for each candidates
+opercentage = "{:.3f}".format((ovotes/total_votes)*100) # Calculate O'Tolley percentage and adding format
+kpercentage = "{:.3f}".format((kvotes/total_votes)*100) # Calculate Kahn percentage and adding format
+lpercentage = "{:.3f}".format((lvotes/total_votes)*100) # Calculate Li percentage and adding format
+cpercentage = "{:.3f}".format((cvotes/total_votes)*100) # Calculate Correy percentage and adding format
+winner = max(w_votes) #Identify the winner by votes
+i_winner = w_votes.index(winner) #Getting the index of the winner to get it's name
 
+# Print results in terminal
 print ("Election Results")
 print ("-------------------------")
 print (f"Total Votes: {total_votes}")
